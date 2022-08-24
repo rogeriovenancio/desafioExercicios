88fa40ba637e416604c686630e03d3363a7d64a3
Rogerio <rogeriorvs@hotmail.com>
2022-08-23 21:16:57 -0300

    four commit

---
+++ desafio3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class desafio3 {
+    /**
+     * 3. Criar um programa que leia o peso e a altura do usuário e imprima no
+     * console o IMC.
+     */
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o peso:");
+        double peso = scanner.nextDouble();
+
+        System.out.println("Digite a altura:");
+        double altura = scanner.nextDouble();
+
+        double imc = peso / (altura * altura);
+
+        System.out.print("IMC: " + imc);
+
+        scanner.close();
+    }
+
+}
